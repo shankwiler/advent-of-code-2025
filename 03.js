63534d603eb8882ec036a9c918dc3f6eae4080e1
Kyle Shankwiler <kyleshankwiler@gmail.com>
2025-12-02 22:08:40 -0800

    03

---
+++ 03.js
@@ -0,0 +1,67 @@
+/*
+The solution for part two feels a little heavy handed -- I bet there's something
+simpler.
+
+But in reality the runtime isn't bad: O((size of row) * (number of allowed digits)).
+
+We're answering this question for each position in the row: what's the best possible
+number I could construct with myself and the digits before me in the row, given some
+number of allowed digits from 1-12.
+*/
+
+// part one
+(() => {
+  const lines = document
+    .querySelector("pre")
+    .innerText.split("\n")
+    .filter((e) => !!e);
+
+  const rows = lines.map((line) =>
+    Array.from(line).map((digit) => parseInt(digit))
+  );
+
+  let total = 0;
+  for (const row of rows) {
+    let best = -1;
+    let maxDigitBefore = row[0];
+    for (let i = 1; i < row.length; i++) {
+      best = Math.max(best, maxDigitBefore * 10 + row[i]);
+      maxDigitBefore = Math.max(maxDigitBefore, row[i]);
+    }
+    total += best;
+  }
+  return total;
+})();
+
+// part two
+(() => {
+  const lines = document
+    .querySelector("pre")
+    .innerText.split("\n")
+    .filter((e) => !!e);
+
+  const solve = (row, index, digits, cache) => {
+    if (index === -1 || digits === 0) {
+      return 0;
+    }
+    const key = JSON.stringify({ index, digits });
+    if (key in cache) {
+      return cache[key];
+    }
+    const ifInclude =
+      solve(row, index - 1, digits - 1, cache) * 10 + row[index];
+    const ifNotInclude = solve(row, index - 1, digits, cache);
+    cache[key] = Math.max(ifInclude, ifNotInclude);
+    return cache[key];
+  };
+
+  const rows = lines.map((line) =>
+    Array.from(line).map((digit) => parseInt(digit))
+  );
+
+  let total = 0;
+  for (const row of rows) {
+    total += solve(row, row.length - 1, 12, {});
+  }
+  return total;
+})();
